208346c3170bc98f6aefabdfe4c4894dd82f6747
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-24 14:39:42 +0200

    player movement animations

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -11,5 +11,12 @@ func _physics_process(delta):
 
 func movement(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var side = speed * direction
+	if side == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("Idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("Movement")
+		$AnimationTree.set("parameters/Idle/blend_position", side)
+		$AnimationTree.set("parameters/Movement/blend_position", side)
 	velocity = lerp(velocity, speed * direction, pow(2, -40 * delta))
 	move_and_slide()
