af59c3b8add7668fbea31b3a41191e7968fcd592
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-26 23:26:46 +0200

    mixing minigame concept (not done)

--- game/minigames/mixing_minigame/mixing_mini_game.gd
+++ game/minigames/mixing_minigame/mixing_mini_game.gd
@@ -4,9 +4,11 @@ extends CanvasLayer
 
 
 func _ready():
+	get_parent().player.set_physics_process(false)
 	print(get_parent().substance_table.substances)
 
 
 func _on_button_pressed():
+	get_parent().player.set_physics_process(true)
 	get_parent().playing_mini_game = false
 	queue_free()
