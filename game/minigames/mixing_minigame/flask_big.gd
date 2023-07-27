553ae881d99b88bfd1c7896f671c49b0645a0c18
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-27 14:51:32 +0200

    big flask can now hold substances consistently

--- game/minigames/mixing_minigame/flask_big.gd
+++ game/minigames/mixing_minigame/flask_big.gd
@@ -1,4 +1,4 @@
-extends Button
+extends AnimatableBody2D
 
 
 
@@ -7,21 +7,31 @@ func _ready():
 	set_process(false)
 
 func _process(delta):
-	var old_position = global_position
-	global_position = Vector2(get_global_mouse_position().x - ($AnimatableBody2D/Sprite2D.texture.get_width()), get_global_mouse_position().y - ($AnimatableBody2D/Sprite2D.texture.get_height()))
-#	var tween = create_tween()
-#	tween.tween_property(self, "global_position", Vector2(get_global_mouse_position().x - ($AnimatableBody2D/Sprite2D.texture.get_width()), get_global_mouse_position().y - ($AnimatableBody2D/Sprite2D.texture.get_height())), 0.1)
-	if Input.is_action_just_released("rotate_up"):
-		var rotation_tween = create_tween()
-		rotation_tween.tween_property(self, "rotation", rotation+0.4, 0.1)
-	if Input.is_action_just_released("rotate_down"):
-		var rotation_tween2 = create_tween()
-		rotation_tween2.tween_property(self, "rotation", rotation-0.4, 0.1)
+	if Input.is_action_pressed("left_click"):
+		var target = Vector2.ZERO
+		target.x = clamp(get_global_mouse_position().x, 0, get_viewport_rect().size.x)
+		target.y = clamp(get_global_mouse_position().y, 0, get_viewport_rect().size.y)
+		var tween = create_tween()
+		tween.tween_property(self, "global_position", target, 250 * delta)
+		if Input.is_action_just_released("rotate_up"):
+			var rotation_tween = create_tween()
+			rotation_tween.tween_property(self, "rotation", rotation+0.4, 0.1)
+		if Input.is_action_just_released("rotate_down"):
+			var rotation_tween2 = create_tween()
+			rotation_tween2.tween_property(self, "rotation", rotation-0.4, 0.1)
+	elif Input.is_action_just_released("left_click"):
+		var tween = create_tween()
+		tween.tween_property(self, "global_position", global_position, 250 * delta)
+		rotation = 0
+		set_process(false)
+		$Area2D/CollisionShape2D.disabled = true
+		await get_tree().create_timer(0.1).timeout
+		$Area2D/CollisionShape2D.disabled = false
 
 
-func _on_button_down():
+func _on_area_2d_mouse_entered():
 	set_process(true)
 
-func _on_button_up():
-	rotation = 0
-	set_process(false)
+func _on_area_2d_mouse_exited():
+	if !Input.is_action_pressed("left_click"):
+		set_process(false)
