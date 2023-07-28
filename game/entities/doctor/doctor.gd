d7d159b29fce293b531f3aee9f48b3a0380e15b0
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-28 17:52:24 +0200

    doctor, pacient, can hand over substance to doctor

---
+++ game/entities/doctor/doctor.gd
@@ -0,0 +1,40 @@
+extends StaticBody2D
+
+
+
+var player
+
+
+func _ready():
+	set_process(false)
+	$Label.text = "[" + InputMap.action_get_events("put")[0].as_text() + "]"
+	$Label2.text = "Hand over"
+	$Label.visible = false
+	$Label2.visible = false
+	
+
+func _process(delta):
+	if player.can_pick_up == false:
+		$Label.visible = true
+		$Label2.visible = true
+		if Input.is_action_just_pressed("put"):
+			for i in player.get_children():
+				if i.is_in_group("correct"):
+					print("victory")
+				else:
+					print("game over")
+					break
+	else:
+		$Label.visible = false
+		$Label2.visible = false
+
+
+func _on_area_2d_body_entered(body):
+	player = body
+	if player.can_pick_up == false:
+		set_process(true)
+
+func _on_area_2d_body_exited(body):
+	$Label.visible = false
+	$Label2.visible = false
+	set_process(false)
