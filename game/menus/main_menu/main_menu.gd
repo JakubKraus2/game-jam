1b06b5d15c72abe6a918b64fc0e2b02e7ea1dd58
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-30 00:38:59 +0200

    main menu done, escape menu adjustments

--- game/menus/main_menu/main_menu.gd
+++ game/menus/main_menu/main_menu.gd
@@ -8,3 +8,8 @@ func _on_quit_button_pressed():
 	PseudoLoading.get_node("AnimationPlayer").play("change")
 	await PseudoLoading.get_node("AnimationPlayer").animation_finished
 	get_tree().quit()
+
+func _on_select_level_button_pressed():
+	PseudoLoading.get_node("AnimationPlayer").play("change")
+	await PseudoLoading.get_node("AnimationPlayer").animation_finished
+	get_tree().change_scene_to_file("res://game/menus/level_select_menu/level_select_menu.tscn")
