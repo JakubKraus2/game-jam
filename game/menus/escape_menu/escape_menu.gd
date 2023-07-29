377e5b16ff3431ded1c4bbcedfb810802e5ef704
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-29 23:48:22 +0200

    escape menu

--- game/menus/escape_menu/escape_menu.gd
+++ game/menus/escape_menu/escape_menu.gd
@@ -3,13 +3,22 @@ extends CanvasLayer
 
 
 
-func _ready():
-	if Global.current_level != null:
-		$TextureRect/HBoxContainer/Label.text = "Level" + str(Global.current_level)
-
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		if visible == false:
 			$AnimationPlayer.play("open")
 		else:
 			$AnimationPlayer.play("close")
+
+func _on_visibility_changed():
+	if Global.current_level != null:
+		$TextureRect/HBoxContainer/Label.text = "Level" + str(Global.current_level)
+
+
+func _on_resume_button_pressed():
+	$AnimationPlayer.play("close")
+
+func _on_quit_button_pressed():
+	PseudoLoading.get_node("AnimationPlayer").play("change")
+	await PseudoLoading.get_node("AnimationPlayer").animation_finished
+	get_tree().change_scene_to_file("res://game/menus/level_select_menu/level_select_menu.tscn")
