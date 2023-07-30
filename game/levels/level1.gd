5e8b12a07996526b2bfba69e1598800527f9ab06
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-30 17:27:07 +0200

    fixed camera bug, green_box spawns on semi-random positions in level2

--- game/levels/level1.gd
+++ game/levels/level1.gd
@@ -4,3 +4,17 @@ extends LevelParent
 
 var correct_substances = {"Cerna": 0.0, "Modra": 0.0, "Zluta": 0.0}
 var correct_percentage = {"Cerna": 15.0, "Modra": 50.0, "Zluta": 35.0}
+
+
+
+
+func _on_ready():
+	var box_random_position = randi_range(0, 2)
+	match box_random_position:
+		0:
+			$GreenBox.position = Vector2(randi_range(-585, -390), randi_range(310, 210))
+		1:
+			$GreenBox.position = Vector2(randi_range(-245, -80), randi_range(310, 210))
+		2:
+			$GreenBox.position = Vector2(randi_range(400, 530), randi_range(310, 210))
+	print($GreenBox.position)
