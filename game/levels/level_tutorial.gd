4490b010fbb29064299bdb0be1a2a56ce8f58803
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-30 11:31:11 +0200

    minor bug fixes, added dialogue_payer animation in tutorial_mixing

--- game/levels/level_tutorial.gd
+++ game/levels/level_tutorial.gd
@@ -10,6 +10,19 @@ var already_played = false
 var mixing_minigame
 
 
+
+func _on_ready():
+	set_process(false)
+
+func _process(delta):
+	if dialogue_player.current_dialogue_index == 2 && dialogue_player.get_node("NinePatchRect").position.y == 248:
+		var position_tweem = create_tween()
+		position_tweem.tween_property(dialogue_player.get_node("NinePatchRect"), "position:y", dialogue_player.get_node("NinePatchRect").position.y-140, 0.3)
+	elif dialogue_player.current_dialogue_index >= 5 && dialogue_player.get_node("NinePatchRect").position.y != 248:
+		var position_tweem = create_tween()
+		position_tweem.tween_property(dialogue_player.get_node("NinePatchRect"), "position:y", 248, 0.3)
+
+
 func _on_mixing_table_child_entered_tree(node):
 	mixing_minigame = node
 	if $MixingTable.playing_mini_game && already_played == false:
@@ -17,7 +30,9 @@ func _on_mixing_table_child_entered_tree(node):
 		already_played = true
 		await mixing_minigame.ready
 		mixing_minigame.get_node("Button").disabled = true
+		set_process(true)
 
 func _on_child_exiting_tree(node):
 	if node == dialogue_player && already_played == true:
 		mixing_minigame.get_node("Button").disabled = false
+		set_process(false)
