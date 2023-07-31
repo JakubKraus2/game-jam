a8f7218da86e706e78d93cfbac4d5461f72da7e1
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-31 23:17:34 +0200

    quick fix

--- game/levels/level2.gd
+++ game/levels/level2.gd
@@ -4,6 +4,7 @@ extends LevelParent
 
 var correct_substances = {"Water": 0.0, "Flower Powder": 0.0}
 var correct_percentage = {"Water": 60.0, "Flower Powder": 20.0}
+var already_played = false
 
 
 func _on_ready():
@@ -12,6 +13,8 @@ func _on_ready():
 
 
 func _on_child_exiting_tree(node):
-	if node == $DialoguePlayer:
-		$Player.set_physics_process(true)
-		$Fuses.get_node("BlackOutTimer").start()
+	if already_played == false:
+		if node == $DialoguePlayer:
+			$Player.set_physics_process(true)
+			$Fuses.get_node("BlackOutTimer").start()
+			already_played = true
