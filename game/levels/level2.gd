69e741c76bff8b5b53f5e301d9dda4c86f1f7a1a
jakub.kraus2 <jakub.kraus007@seznam.cz>
2023-07-31 23:00:39 +0200

    fuse_box tutorial added

--- game/levels/level2.gd
+++ game/levels/level2.gd
@@ -4,3 +4,14 @@ extends LevelParent
 
 var correct_substances = {"Water": 0.0, "Flower Powder": 0.0}
 var correct_percentage = {"Water": 60.0, "Flower Powder": 20.0}
+
+
+func _on_ready():
+	$DialoguePlayer.play()
+	$Player.set_physics_process(false)
+
+
+func _on_child_exiting_tree(node):
+	if node == $DialoguePlayer:
+		$Player.set_physics_process(true)
+		$Fuses.get_node("BlackOutTimer").start()
